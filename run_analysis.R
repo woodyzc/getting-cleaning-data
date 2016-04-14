256d278075a37422085094f380e045d59b04c091
Chong Zhang <woodyzc@gmail.com>
2016-04-13 22:02:16 -0400

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+#down load the data from the link. 
+#open data and merge two data sets together.First commit.
+
+library(dplyr)
+
+# load both test and train data.
+testData<- read.table('test/X_test.txt')
+trainData<- read.table('train/X_train.txt')
+
+# load both subject data
+testSubject<- read.table('test/subject_test.txt')
+trainSubject<- read.table('train/subject_train.txt') 
+
+# combind them together
+allData<- rbind(testData,trainData)
+allSubject<- rbind(testSubject,trainSubject)
+
+# load all the features and change the variable names to feature names.
+feature<- read.table('features.txt')
+names(allData)<- feature$V2
+
+# add a column named subject to allData
+allData<- cbind(allData,subject=allSubject$V1)
+
+# load both labels and combind them together
+testLable<- read.table('test/y_test.txt')
+trainLable<- read.table('train/y_train.txt')
+allLable<- rbind(testLable,trainLable)
+
+# update the allData with a column named label
+allData<- cbind(allData,lable=allLable$V1)
+
+# load activity labels
+activity<- read.table('activity_labels.txt')
+names(activity)<-c('V1','activity')
+
+#merge allData and activies labels data frame.
+allData<- merge(x=allData,y=activity,by.y='V1',by.x='lable')
+allData<- tbl_df(allData)
+
+# find variables with only meansurements on the mean and std
+mean_std_Col<- grep('std\\(\\)|mean\\(\\)',names(allData),value = TRUE)
+meanAndStd<- allData[,c(mean_std_Col,"lable",'activity',"subject")]
+
+
